acbdac9657f24bcc511d9b6cb723cb88a9a58912
yanmulin <yanmulin@codegrind.club>
2019-09-22 17:02:25 +0800

    add cors

--- Zepplin/app/__init__.py
+++ Zepplin/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from config import config
 
 db = SQLAlchemy()
@@ -12,6 +13,7 @@ def create_app(config_name='default'):
     db.init_app(app)
 
     from .api import api as api_blueprint
+    CORS(api_blueprint) 
     app.register_blueprint(api_blueprint)
 
     return app
