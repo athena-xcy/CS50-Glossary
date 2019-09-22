4d74241e65c43729028779858328696e6872805d
yanmulin <yans1996@outlook.com>
2019-09-22 21:19:18 +0800

    change detail form layout

--- Glossary/components/GlossaryForm.js
+++ Glossary/components/GlossaryForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import { Form, Row, Col, Button, Input, Descriptions, Card, Icon, Spin, message } from 'antd';
+import { Form, Row, Col, Button, Input, Descriptions, Card, Icon, Spin, message, Typography } from 'antd';
 import moment from 'moment';
 import { capitalizeWord } from '../stores/GlossaryStore';
 
@@ -11,6 +11,8 @@ const updateFormResponsive = {
     lg: { span: 10 },
     xl: { span: 10 }
 };
+
+const { Title, Text } = Typography;
 
 @inject('glossaryStore')
 @observer
@@ -74,10 +76,14 @@ class GlossaryForm extends Component {
 
         const isEditing = this.state.isEditing || glossary.gid <= 0;
 
+        const cardUpperRightButton = isEditing ?
+            <span onClick={toggleEdit} style={{ color: "#1084f5"}}><Icon type="table" />  详情</span> :
+            <span onClick={toggleEdit} style={{ color: "#1084f5"}} ><Icon type="edit" />  编辑</span>;
+
         return (
             <Card 
             loading={this.state.loading}
-            extra={<Icon type={ isEditing ? "table" : "edit" } onClick={toggleEdit} style={{ fontSize: '1.2em', color: "#1084f5"}}/>}
+            extra={cardUpperRightButton}
             title={<span>{capitalizeWord(glossary.word)} <span style={{ fontSize: '0.6em'}}>{glossary.gid > 0 ? '#' + glossary.gid : '(未添加)' }</span></span>}
             headStyle={{ fontSize: '1.6em'}}
             bordered={true}
@@ -92,17 +98,32 @@ class GlossaryForm extends Component {
                         <Button type="primary" htmlType="submit" style={{ float: 'right' }}>提交修改</Button>
                     </Form>)
                 :
-                    (<Descriptions  
-                    column={{ md: 4, sm: 2, xs: 1}}
-                    layout='vertical'
-                    bordered>
-                        <Descriptions.Item span={4} label="译名">{glossary.translation}</Descriptions.Item>
-                        <Descriptions.Item label="添加人">{glossary.creator}</Descriptions.Item>
-                        <Descriptions.Item label="添加时间">{moment.unix(glossary.createtime).fromNow()}</Descriptions.Item>
-                        <Descriptions.Item label="上次修改人">{glossary.updater}</Descriptions.Item>
-                        <Descriptions.Item label="上次修改时间">{moment.unix(glossary.updatetime).fromNow()}</Descriptions.Item>
-                        <Descriptions.Item span={4} label="备注">{glossary.remark}</Descriptions.Item>
-                    </Descriptions>)
+                    (<div>
+                        <Descriptions  
+                        column={1}
+                        bordered>
+                            <Descriptions.Item label="译名">
+                                <Text 
+                                copyable 
+                                style={{ fontSize: '1.2em', fontWeight: 500, width: '100%' }}
+                                >
+                                {glossary.translation}
+                                </Text>
+                            </Descriptions.Item>
+                            <Descriptions.Item label="备注"><Text>{glossary.remark}</Text></Descriptions.Item>
+                        </Descriptions>
+                        <Descriptions 
+                        column={{ lg: 4, md: 2, sm: 2, xs: 2}} 
+                        layout='horizontal' 
+                        size='small' 
+                        style={{ textAlign: "center", marginTop: 20, fontSize: '0.8', fontWeight: 300 }}
+                        >
+                            <Descriptions.Item label="添加人">{glossary.creator}</Descriptions.Item>
+                            <Descriptions.Item label="添加时间">{moment.unix(glossary.createtime).fromNow()}</Descriptions.Item>
+                            <Descriptions.Item label="上次修改人">{glossary.updater}</Descriptions.Item>
+                            <Descriptions.Item label="上次修改时间">{moment.unix(glossary.updatetime).fromNow()}</Descriptions.Item>
+                        </Descriptions>
+                    </div>)
                 }
             </Card>
         );
