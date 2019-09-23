552fe7db6889822ac3dec192e38f18a38149121a
yanmulin <yans1996@outlook.com>
2019-09-23 09:48:16 +0800

    reorganize directory

--- Zepplin/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-cd /home/yanmulin/CS50-Glossary/Zepplin/
-pyenv activate zepplin
-gunicorn -w4 -blocalhost:5000 manage:app
-
